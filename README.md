705a15e3b250325cd1cc204f778c34d4bcef71bd
IndraHardika <92752408+KZdra@users.noreply.github.com>
2024-04-18 20:30:37 +0700

    Uploaded

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# PokeVue3
+
+fetching pokemon from pokeapi 
+gatau anjg tsnya jalan gatau nggak
+
+
+
+
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+   <div class="p-14 ">
+    <span class="flex justify-center text-4xl text-yellow-700">
+     Pokémon Picker&nbsp;
+    </span>
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+
+</style>
